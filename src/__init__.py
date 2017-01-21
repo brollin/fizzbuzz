74dc3ebd5364c42033d3dc6152a4187a519bd57e
brollin <rollin.ben@gmail.com>
2017-01-21 00:55:55 -0800

    Added package, module, and function docstrings.

---
+++ src/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    __init__.py
+
+    fizzbuzz
+    ~~~~~~~~
+
+    A package for generating a set of Fibonacci
+    numbers, and replacing the certain numbers
+    with "Fizz", "Buzz", "FizzBuzz", or "BuzzFizz".
+
+"""
+
+__license__ = 'MIT'
+__version__ = '1.0'
+__author__ = 'Ben Rollin'
+
+from .fizzbuzz import fibonacci_fizzbuzz
+
---
+++ src/fizzbuzz.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    fizzbuzz.py
+
+    A module for generating the first n Fibonacci
+    numbers as F(n), displaying:
+        "Buzz" when F(n) is divisible by 3,
+        "Fizz" when F(n) is divisible by 5,
+        "FizzBuzz" when F(n) is divisible by 15,
+        "BuzzFizz" when F(n) is prime,
+        the value F(n) otherwise.
+
+"""
+
+#imports
+
+def fibonacci_fizzbuzz(output = 100):
+    """
+    Print a Fibonacci series F(n), displaying:
+        "Buzz" when F(n) is divisible by 3,
+        "Fizz" when F(n) is divisible by 5,
+        "FizzBuzz" when F(n) is divisible by 15,
+        "BuzzFizz" when F(n) is prime,
+        the value F(n) otherwise.
+
+    output -- number of lines of output (default 100)
+    """
+    pass 
+
+def generate_fibonacci_series(count):
+    """
+    Generate a Fibonacci series and return as a list.
+
+    count -- the count of Fibonacci numbers
+    """
+    pass
+
+if __name__ == "__main__":
+    print "Welcome to FizzBuzz."
+
+    fizzbuzz()
+
